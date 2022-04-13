d76a1ac780d6992f75fbf405e20fd00c3f3993fe
IlhombekUbaydullayev <ubaydullaevilhombek682@gmail.com>
2022-04-13 17:09:23 +0500

    message

--- app/src/main/java/com/example/android_instagram_clone/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/android_instagram_clone/fragments/BaseFragment.kt
@@ -2,10 +2,53 @@ package com.example.android_instagram_clone.fragments
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDialog
 import androidx.fragment.app.Fragment
+import com.example.android_instagram_clone.R
 import com.example.android_instagram_clone.activity.SignInActivity
 
 open class BaseFragment: Fragment() {
+
+    var progressDialog: AppCompatDialog? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return view
+    }
+
+    fun showLoading(activity: Activity?) {
+        if (activity == null) return
+
+        if (progressDialog != null && progressDialog!!.isShowing()) {
+
+        } else {
+            progressDialog = AppCompatDialog(activity, R.style.CustomDialog)
+            progressDialog!!.setCancelable(false)
+            progressDialog!!.setCanceledOnTouchOutside(false)
+            progressDialog!!.getWindow()!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            progressDialog!!.setContentView(R.layout.custom_progress_dialog)
+            val iv_progress = progressDialog!!.findViewById<ImageView>(R.id.iv_progress)
+            val animationDrawable = iv_progress!!.getDrawable() as AnimationDrawable
+            animationDrawable.start()
+            if (!activity.isFinishing) progressDialog!!.show()
+        }
+    }
+
+    protected fun dismissLoading() {
+        if (progressDialog != null && progressDialog!!.isShowing) {
+            progressDialog!!.dismiss()
+        }
+    }
 
     fun callSigInActivity(activity: Activity) {
         val intent = Intent(context, SignInActivity::class.java)
